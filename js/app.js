e63c9b1da555eda95f5f19fd8916793da43545bc
Emily Main <emilymuirmain@gmail.com>
2016-10-10 12:42:10 -0700

    comparing keys

--- js/app.js
+++ js/app.js
@@ -3,31 +3,72 @@ $(document).ready(function(){
 console.log("Connected!")
 //
 //
-// var aTreble = assets/ATreble.png;
-// var bTreble = assets/BTreble.png;
-// var bTreble2 = assets/BTreble2.png;
-// var cTreble2 = assets/BTreble2.png;
-// var dTreble1 =assets/DTreble1.png;
-// var dTreble2 = assets/DTreble2.png;
-// var eTreble1 = assets/ETreble1.png;
-// var fTreble = assets/ETreble1.png;
-// var gTreble = assets/GTreble.png;
 
-// switch( image){
-//   case
-// }
 
-// if (){
-//   $('#keyF').on('click', function(){
-//     alert("Correct!");
-//   } else {
-//     alert("Try Again!")
-//   })
-// }
+var aTreble = {
+  img: 'assets/ATreble.png',
+  win:['keyA']
+};
+
+var bTreble = {
+  img: 'assets/BTreble.png',
+  win:['keyB']};
+
+var bTreble2 = {
+  img: 'assets/BTreble2.png',
+  win: ['keyB']};
+
+var cTreble2 = {
+  img: 'assets/BTreble2.png',
+  win: ['keyB']};
+
+var dTreble1 = {
+  img: 'assets/DTreble1.png',
+  win: ['keyD']};
+
+var dTreble2 = {
+  img: 'assets/DTreble2.png',
+  win: ['keyD']};
+
+
+var eTreble1 = {
+ img: 'assets/ETreble1.png',
+ win: ['keyE']};
+
+var fTreble = {
+  img:'assets/FTreble.png',
+  win: ['keyF']};
+
+var gTreble = {
+  img: 'assets/GTreble.png',
+  win: ['keyG']};
+
+var currentCombo = fTreble
+var playedKeys = []
+
 
 $('.keys').on('click', function(){
+  playedKeys.push($(this).attr('id'))
   console.log($(this).attr('id'))
+  console.log(playedKeys)
+  console.log(currentCombo.win)
+  if (playedKeys.length == currentCombo.win.length){
+    for (var i = 0; i<playedKeys.length; i++){
+      if (playedKeys[i] != currentCombo.win[i]){
+        console.log('Try Again!')
+        playedKeys=[]
+
+        return;
+      }
+    }
+    console.log('you win!')
+  }
 })
+
+
+
+
+
 
 
 // $( "#whiteKeys" ).click();
