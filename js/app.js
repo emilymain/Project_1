38af4d9df28f8008e8e19f54b76e65c653e62a02
Emily Main <emilymuirmain@gmail.com>
2016-10-10 12:11:49 -0700

    key ids

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
+$(document).ready(function(){
+
 console.log("Connected!")
+//
+//
+// var aTreble = assets/ATreble.png;
+// var bTreble = assets/BTreble.png;
+// var bTreble2 = assets/BTreble2.png;
+// var cTreble2 = assets/BTreble2.png;
+// var dTreble1 =assets/DTreble1.png;
+// var dTreble2 = assets/DTreble2.png;
+// var eTreble1 = assets/ETreble1.png;
+// var fTreble = assets/ETreble1.png;
+// var gTreble = assets/GTreble.png;
+
+// switch( image){
+//   case
+// }
+
+// if (){
+//   $('#keyF').on('click', function(){
+//     alert("Correct!");
+//   } else {
+//     alert("Try Again!")
+//   })
+// }
+
+$('.keys').on('click', function(){
+  console.log($(this).attr('id'))
+})
+
+
+// $( "#whiteKeys" ).click();
+
+
+
+
+
+
+
+})
